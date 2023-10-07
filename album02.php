415445efab8c1bc85bd735cbb79734340ce57962
kurushin.fedor <kurushin.fedor@wb.ru>
2023-10-07 20:47:21 +0300

    задача А

--- album02.php
+++ album02.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles/main.css">
+    <link rel="stylesheet" href="styles/main2.css">
     <title>Album</title>
     <script src="scripts/jquery-3.7.1.js"></script>
     <script>
