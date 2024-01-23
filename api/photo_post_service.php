5d57e1923f833beebfc7b2e105d2097f3735223b
fkurushin <fkurushin@ya.ru>
2024-01-23 23:20:09 +0300

    add POST to server

--- api/photo_post_service.php
+++ api/photo_post_service.php
@@ -1,7 +1,17 @@
 <?php
-    $title = $_REQUEST['title'];
-    $description = $_REQUEST['description'];
-    $thumb = $_REQUEST['thumb'];
-    $photo = $_REQUEST['photo'];
+    // // GET
+    // $title = $_REQUEST['title'];
+    // $description = $_REQUEST['description'];
+    // $thumb = $_REQUEST['thumb'];
+    // $photo = $_REQUEST['photo'];
+    // include '../db/photo_utils.inc';
+    // post_new_photo($title, $description, $thumb, $photo);
+    
+    // POST
+    $data = json_decode(file_get_contents('php://input'), true);
+    $title = $data['title'];
+    $description = $data['description'];
+    $thumb = $data['thumb'];
+    $photo = $data['photo'];
     include '../db/photo_utils.inc';
     post_new_photo($title, $description, $thumb, $photo);
